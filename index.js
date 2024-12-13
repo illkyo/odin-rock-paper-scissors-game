fe3a32692c3df702039a63cd628784d2ceb5fc80
illkyo <82893863+4thjonasbrother@users.noreply.github.com>
2024-12-13 22:36:56 +0500

    feat: add computer choice function with comments

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 console.log("This is a test");
 
 // computer logic
+  // create a function
+  function getComputerChoice() {
+
+    // generate random number between 0, 1, 2 and store in variable
+    let randomNum =  Math.floor(Math.random() * 3);
+
+    // random number decides the computer's play and stores the choice in a variable
+    let computerChoice;
+
+    // 0 denotes 'rock'
+    // 1 denotes 'paper'
+    // 2 denotes 'scissors'
+    switch(randomNum) {
+      case 0:
+        computerChoice = 'Rock';
+        break
+      case 1:
+        computerChoice = 'Paper';
+        break
+      case 2:
+        computerChoice = 'Scissors';
+        break
+    }
+
+    // return the variable containing choice
+    return computerChoice;
+
+  }
+  // output resulting choice to console
+  console.log(getComputerChoice());
 
 // user logic
 
