6b6e2a05faa7204c9abb3a16d1324297426ff7b9
illkyo <82893863+4thjonasbrother@users.noreply.github.com>
2024-12-14 00:48:06 +0500

    fix: change indentation for readability

--- index.js
+++ index.js
@@ -1,85 +1,85 @@
 console.log("This is a test");
 
 // computer logic
-  // create a function
-  function getComputerChoice() {
+// create a function
+function getComputerChoice() {
 
-    // generate random number between 0, 1, 2 and store in variable
-    let randomNum =  Math.floor(Math.random() * 3);
+  // generate random number between 0, 1, 2 and store in variable
+  let randomNum =  Math.floor(Math.random() * 3);
 
-    // random number decides the computer's play and stores the choice in a variable
-    let computerChoice;
+  // random number decides the computer's play and stores the choice in a variable
+  let computerChoice;
 
+  // 0 denotes 'rock'
+  // 1 denotes 'paper'
+  // 2 denotes 'scissors'
+  switch(randomNum) {
+    case 0:
+      computerChoice = 'Rock';
+      break
+    case 1:
+      computerChoice = 'Paper';
+      break
+    case 2:
+      computerChoice = 'Scissors';
+      break
+  }
+
+  // return the variable containing choice
+  return computerChoice;
+
+}
+// output resulting choice to console
+console.log(`Computer chooses ${getComputerChoice()}!`);
+
+// user logic
+// create a function
+function getHumanChoice() {
+
+  // create a variable storing user input
+  let promptInput;
+  // create a variable that translates it into rock, paper or scissors
+  let userChoice;
+
+  // keep asking user until their input evaluates to rock, paper or scissors
+  while (userChoice === undefined) {
+    // ask user to input a value of either 0, 1 or 2
+    promptInput = prompt("Rock = 0, Paper = 1, Scissors = 2", "");
+
+    // user input conversion
     // 0 denotes 'rock'
     // 1 denotes 'paper'
     // 2 denotes 'scissors'
-    switch(randomNum) {
-      case 0:
-        computerChoice = 'Rock';
+    // if cancel is clicked in prompt window game is exited
+    switch(promptInput) {
+      case '0':
+        userChoice = 'Rock';
         break
-      case 1:
-        computerChoice = 'Paper';
+      case '1':
+        userChoice = 'Paper';
         break
-      case 2:
-        computerChoice = 'Scissors';
+      case '2':
+        userChoice = 'Scissors';
+        break
+      case null:
+        userChoice = 'EXIT'
+        alert("Game Exited.")
+        break
+      default:
+        userChoice = undefined;
+        alert("Only values 0, 1 or 2 are accepted. Please enter either 0, 1 or 2");
         break
     }
-
-    // return the variable containing choice
-    return computerChoice;
-
-  }
-  // output resulting choice to console
-  console.log(`Computer chooses ${getComputerChoice()}!`);
-
-// user logic
-  // create a function
-  function getHumanChoice() {
-
-    // create a variable storing user input
-    let promptInput;
-    // create a variable that translates it into rock, paper or scissors
-    let userChoice;
-
-    // keep asking user until their input evaluates to rock, paper or scissors
-    while (userChoice === undefined) {
-      // ask user to input a value of either 0, 1 or 2
-      promptInput = prompt("Rock = 0, Paper = 1, Scissors = 2", "");
-
-      // user input conversion
-      // 0 denotes 'rock'
-      // 1 denotes 'paper'
-      // 2 denotes 'scissors'
-      // if cancel is clicked in prompt window game is exited
-      switch(promptInput) {
-        case '0':
-          userChoice = 'Rock';
-          break
-        case '1':
-          userChoice = 'Paper';
-          break
-        case '2':
-          userChoice = 'Scissors';
-          break
-        case null:
-          userChoice = 'EXIT'
-          alert("Game Exited.")
-          break
-        default:
-          userChoice = undefined;
-          alert("Only values 0, 1 or 2 are accepted. Please enter either 0, 1 or 2");
-          break
-      }
-    }
-
-    // return the variable containing choice
-    return userChoice;
   }
 
-  // output resulting choice to console
-  console.log(`Human chooses ${getHumanChoice()}!`);
+  // return the variable containing choice
+  return userChoice;
+}
 
-// score tracker 
+// output resulting choice to console
+console.log(`Human chooses ${getHumanChoice()}!`);
+
+// score tracker
 
 // single round logic
 
